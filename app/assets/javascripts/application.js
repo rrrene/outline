7cfa6bf01972034fa16a12442d2582b99be4360b
René Föhring <rf@neopoly.de>
2012-01-24 14:47:32 +0100

    Add sortability to content_items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-$(document).ready(function(){
+
+$(function(){
   $('.content-items').sortable({
     axis: 'y',
     dropOnEmpty: false,
