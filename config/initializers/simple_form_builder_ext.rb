e8c7e0c58377db529bf00bc34e80f4c5721c2bcd
René Föhring <rf@bamaru.de>
2012-01-27 13:04:16 +0100

    Fix bug in SimpleForm::FormBuilder extension

--- config/initializers/simple_form_builder_ext.rb
+++ config/initializers/simple_form_builder_ext.rb
@@ -2,11 +2,9 @@
 
 class SimpleForm::FormBuilder
   def buttons(locals = {})
-    resource = locals[:resource]
-    options = {:model => resource.class.model_name.human}
-    resource_key = resource.class.to_s.underscore
-    create_or_update = resource.try(:new_record?) ? :create : :update
-    locals[:submit_text] ||= @template.tt("helpers.submit.#{resource_key}.#{create_or_update}", "helpers.submit.#{create_or_update}", options)
+    options = object.present? ? {:model => object.class.model_name.human} : {}
+    create_or_update = object.try(:new_record?) ? :create : :update
+    locals[:submit_text] ||= @template.tt("helpers.submit.#{object.class.to_s.underscore}.#{create_or_update}", "helpers.submit.#{create_or_update}", options)
     locals[:cancel_text] ||= @template.tt("helpers.cancel", options)
     locals[:remote] = !!locals[:remote]
     @template.render :partial => "shared/form_buttons", :locals => locals
