2dd61bdc6cd3bf6da4a4a051df8ac60a20c541b8
René Föhring <rf@bamaru.de>
2012-03-17 09:06:12 +0100

    Add tests for Domain

--- test/unit/domain_test.rb
+++ test/unit/domain_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DomainTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should give tags" do
+    domain = Domain.first
+    assert !domain.tags.nil?
+  end
+
+  test "should give tags for pages" do
+    domain = Domain.first
+    assert !domain.tags_for(:pages).nil?
+  end
+
 end
