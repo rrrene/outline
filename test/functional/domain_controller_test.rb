37d891fb9bd2daa5b7661828aa826a63798f253c
René Föhring <rf@bamaru.de>
2012-03-17 09:01:52 +0100

    Add domain controller tests

--- test/functional/domain_controller_test.rb
+++ test/functional/domain_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class DomainControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should GET dashboard" do
+    with_login do |user|
+      get :dashboard
+      assert_response :success
+    end
+  end
+
+  test "should GET settings" do
+    with_login do |user|
+      get :settings
+      assert_response :success
+    end
+  end
+
+  test "should update settings" do
+    with_login do |user|
+      hash = {:title => "New Title", :theme => "outline"}
+      put :settings, :domain => hash
+      assert_not_nil assigns["domain"]
+      hash.each do |attribute, value|
+        assert_equal value, assigns["domain"].send(attribute)
+      end
+      assert_response :success
+    end
+  end
+
 end
