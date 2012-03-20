8797409745b85e6b8bb3ecc29831b3c1c720cb85
René Föhring <rf@neopoly.de>
2012-03-20 11:55:11 +0100

    Move buttons helper method from FormBuilder to form_helper

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,27 @@
 module FormHelper
+  def buttons(object, locals = {})
+    options = object.present? ? {:model => object.class.model_name.human} : {}
+    locals[:submit_text] ||= submit_text(object, options)
+    locals[:cancel_text] ||= cancel_text(options)
+    locals[:remote] = !!locals[:remote]
+    locals[:resource] = object
+    render :partial => "shared/form_buttons", :locals => locals
+  end
+
   def cancel_path(resource = nil)
     if resource
       resource_path(resource)
     else
       collection_path
     end
+  end
+
+  def cancel_text(options = {})
+    tt("helpers.cancel", options)
+  end
+
+  def submit_text(object, options = {})
+    create_or_update = object.try(:new_record?) ? :create : :update
+    tt("helpers.submit.#{object.class.to_s.underscore}.#{create_or_update}", "helpers.submit.#{create_or_update}", options)
   end
 end
--- config/initializers/simple_form_builder_ext.rb
+++ config/initializers/simple_form_builder_ext.rb
@@ -2,12 +2,6 @@
 
 class SimpleForm::FormBuilder
   def buttons(locals = {})
-    options = object.present? ? {:model => object.class.model_name.human} : {}
-    create_or_update = object.try(:new_record?) ? :create : :update
-    locals[:submit_text] ||= @template.tt("helpers.submit.#{object.class.to_s.underscore}.#{create_or_update}", "helpers.submit.#{create_or_update}", options)
-    locals[:cancel_text] ||= @template.tt("helpers.cancel", options)
-    locals[:remote] = !!locals[:remote]
-    locals[:resource] = object
-    @template.render :partial => "shared/form_buttons", :locals => locals
+    @template.buttons object, locals
   end
 end
