bc171f3687cd9675968d0cba83046c743101f7d7
René Föhring <rf@neopoly.de>
2012-04-05 15:00:43 +0200

    Remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,3 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-
-Domain.create!(:title => "Outline") if Domain.count == 0
-
-Domain.first.users.create(:login => "admin", :password => "admin", :password_confirmation => "admin", :email => "example@example.org")
-Domain.first.users.create(:login => "demo", :password => "demo", :password_confirmation => "demo", :email => "example2@example.org")
-
-Project
-
-user = Domain.first.users.first
-["Diploma", "Disseration", "Award Show", "Trip to Australia"].each do |title|
-  opts = {:title => title}
-  record = Domain.first.projects.create(opts)
-  record.user = user
-  record.save
-end
-
-Page
-
-user = Domain.first.users.first
-["A new page", "Another page", "Homepage", "Turn the page"].each do |title|
-  opts = {:title => title}
-  record = Domain.first.pages.create(opts)
-  record.user = user
-  record.save
-end
