32cbd9e871fb4a4caa578dca91fb3cfe9cf395a9
René Föhring <rf@neopoly.de>
2012-04-12 11:38:39 +0200

    Add tests for welcome_controller

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,4 +1,24 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  test "should GET index with login" do
+    with_login do |user|
+      get :index
+      assert_response :redirect
+      assert_redirected_to :controller => "activities"
+    end
+  end
+
+  test "should GET index without login" do
+    get :index
+    assert_response :redirect
+    assert_redirected_to "/login"
+  end
+
+  test "should GET index without login or existing users" do
+    Domain.destroy_all
+    get :index
+    assert_response :redirect
+    assert_redirected_to "/setup"
+  end
 end
