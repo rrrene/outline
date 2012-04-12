35ab47c8545f6908d7b93b9914baf5788a2752ed
René Föhring <rf@neopoly.de>
2012-04-12 12:12:08 +0200

    Add tests to quick_jump_target_test

--- test/unit/quick_jump_target_test.rb
+++ test/unit/quick_jump_target_test.rb
@@ -18,4 +18,22 @@ class QuickJumpTargetTest < ActiveSupport::TestCase
     assert_equal new_title, project.quick_jump_target.phrase
   end
 
+  test "should update QuickJumpTarget resource on request" do
+    resource = QuickJumpTarget.first.resource
+    resource.quick_jump_target.destroy
+    resource.reload
+
+    chain = QuickJumpTarget.where(:resource_type => resource.class, :resource_id => resource.id)
+    assert_equal 0, chain.count
+    QuickJumpTarget.update resource
+    assert_equal 1, chain.count
+  end
+
+  test "should update QuickJumpTarget model on request" do
+    QuickJumpTarget.destroy_all
+    assert_equal 0, QuickJumpTarget.count
+    QuickJumpTarget.update_all Project
+    assert QuickJumpTarget.count > 0
+  end
+
 end
