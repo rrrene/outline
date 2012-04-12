b59e9f3b4225bfb409c84e7245eb986a445a3797
René Föhring <rf@neopoly.de>
2012-04-12 11:12:29 +0200

    Add tests for quick_jump_target_controller

--- test/functional/quick_jump_targets_controller_test.rb
+++ test/functional/quick_jump_targets_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class QuickJumpTargetsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    QuickJumpTarget.update_all Project, Page
+  end
+
+  test "should render json on index" do
+    with_login do |user|
+      get :index, :format => :json, :query => "first"
+      @data = assigns['data']
+      assert_not_nil @data
+      assert !@data.empty?
+      assert @data.any? { |item| item["type"] == "Project" }
+      assert @data.any? { |item| item["type"] == "Page" }
+    end
+  end
+
+  test "should render json on pages" do
+    with_login do |user|
+      get :pages, :format => :json, :query => "first"
+      @data = assigns['data']
+      assert_not_nil @data
+      assert !@data.empty?
+      assert @data.all? { |item| item["type"] == "Page" }
+    end
+  end
 end
