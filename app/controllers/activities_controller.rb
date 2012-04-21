0ee66b8975b41fb6edf825646263b3288c7f7da0
René Föhring <rf@bamaru.de>
2012-04-21 14:00:21 +0200

    Render activity on activities#index per day

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,18 +3,30 @@ class ActivitiesController < ApplicationController
   authorized_resources
 
   def index
+    @start_time = params[:start_time].presence.try(:to_i) || activity_chain.first.created_at.midnight.to_i
+    @end_time = @start_time + 1.day.to_i
     @activity = Activity.new
-    @activities = current_domain.activities.where(["verb NOT IN (?)", %w(read delete)])
-                                            .group("resource_type, resource_id")
-                                            .order("id DESC")
-                                            .limit(ACTIVITIES_PER_PAGE)
-    @activities = @activities.map do |activity| 
-      Activity.where(:resource_type => activity.resource_type, :resource_id => activity.resource_id)
-              .where(["verb NOT IN (?)", %w(read delete)])
-              .order("id DESC")
-              .first
+    @activities = activities_between(@start_time, @end_time)
+    if prev_activity = activity_chain.where("created_at < ?", Time.at(@start_time)).first
+      @prev_time = prev_activity.created_at.midnight.to_i
     end
-    @activities.sort_by!(&:id).reverse!
+    if next_activity = activity_chain.where("created_at >= ?", Time.at(@end_time)).first
+      @next_time = next_activity.created_at.midnight.to_i
+    end
+  end
 
+  def activity_chain
+    current_domain.activities.where(["verb NOT IN (?)", %w(read delete)])
+    .order("id DESC")
+  end
+
+  def activities_between(start_time, end_time)
+    activities = activity_chain.group("resource_type, resource_id").where("created_at >= ? AND created_at < ?", Time.at(start_time), Time.at(end_time))
+    activities = activities.map do |activity|
+      activity_chain.where(:resource_type => activity.resource_type, :resource_id => activity.resource_id)
+      .where("created_at >= ? AND created_at < ?", Time.at(start_time), Time.at(end_time))
+      .first
+    end
+    activities.sort_by(&:id).reverse
   end
 end
