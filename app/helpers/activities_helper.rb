d9444f85dfd4519a1e1e36818c04a996d22bbfde
René Föhring <rf@bamaru.de>
2012-05-14 08:13:38 +0200

    Add links to updated projects and pages to activity#index

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -16,4 +16,17 @@ module ActivitiesHelper
       "#{base}.default",
     ]
   end
+
+  def pages_for_activities(collection)
+    content_ids = collection.map { |a| a.content_id }.compact.uniq
+    contents = content_ids.map { |content_id| ::Content.find(content_id) if ::Content.exists?(content_id) }.compact
+    page_ids = contents.map { |c| c.holder_id if c.holder_type == "Page" }.compact.uniq
+    Page.where(:id => page_ids).order("UPPER(title)")
+  end
+
+  def projects_for_activities(collection)
+    contexts = collection.map { |a| a.context_id }.compact.map { |context_id| ::Context.find(context_id) if ::Context.exists?(context_id) }
+    project_ids = contexts.map { |c| c.resource_id if c.resource_type == "Project" }.compact.uniq
+    Project.where(:id => project_ids).order("UPPER(title)")
+  end
 end
