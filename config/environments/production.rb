123f7e45b6d666a838dfc8716bde6272e1ab28ec
René Föhring <rf@bamaru.de>
2012-05-22 21:25:13 +0200

    Precompile all theme related stylesheets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,11 @@ Outline::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # Precompile all theme related stylesheets as well
+  glob = File.join(Rails.root, "app", "assets", "stylesheets", "themes", "**", "all.css*")
+  themes = Dir[glob].map { |f| f.match(/(themes.+all.css)/).to_s }
+  config.assets.precompile += themes
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
